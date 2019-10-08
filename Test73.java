c655b9fedb5bc990a0a2f6de1c71407528c82c80
tangsongbbb <54836568+tangsongbbb@users.noreply.github.com>
2019-10-08 15:49:36 +0800

    Add files via upload

---
+++ Test73.java
@@ -0,0 +1,35 @@
+
+public class Test73 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Enter the size of the list: ");
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		int n = input.nextInt();
+		System.out.print("Enter the contents of the list:");
+		int[] numbers = new int[n];
+		for(int i = 0; i < n; i++)
+			numbers[i] = input.nextInt();
+		System.out.print("The list has " + n + " integers");
+		for(int i = 0; i < n; i++)
+			System.out.print(" " + numbers[i]);
+		System.out.println();
+		boolean flag = isSorted(numbers);
+		if(flag)
+			System.out.println("The list is already sorted");
+		else
+			System.out.println("The list is not sorted");
+	}
+	
+	public static boolean isSorted(int[] list){
+		int f = 1;
+		for(int i = 0; i < list.length - 1; i++){
+			if(list[i] > list[i+1])
+				f = 0;
+		}
+		if(f != 1)
+			return false;
+		else
+			return true;
+	}
+}
