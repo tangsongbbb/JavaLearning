c7275f25b200d4a5090e3b94c07e48f127faefb7
tangsongbbb <54836568+tangsongbbb@users.noreply.github.com>
2019-10-08 14:14:45 +0800

    Add files via upload

---
+++ Test73.java
@@ -0,0 +1,28 @@
+
+public class Test73 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Enter 10 numbers: ");
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		int[] numbers = new int[10];
+		int i, j, s, f;
+		s = 0;
+		for(i = 0; i < 10; i++){
+			int n = input.nextInt();
+			f = 1;
+			for(j = 0; j < 10; j++){
+				if(n == numbers[j])
+					f = 0;
+			}
+			if(f != 0){
+				numbers[s] = n;
+				s++;
+			}
+		}
+		System.out.println("The number of distinct numbers is " + s);
+		System.out.print("The distinct numbers are: ");
+		for(i = 0; i < s; i++)
+			System.out.print(" " + numbers[i]);
+	}
+}
